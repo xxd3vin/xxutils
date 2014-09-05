9a5661bfc380050e154ccd7126951b5ba1c7af8f
chenyang <chenyang@localhost.localdomain>
2014-09-05 17:47:07 +0800

    add sendemail function

--- xxutils.sh
+++ xxutils.sh
@@ -3,7 +3,7 @@
 ##################################################################
 # Purpose: Run command on remote server, return 0 if success.
 # Arguments:
-#   $1 -> IP of remote server
+#   $1 -> IP of remote server.
 #   $2 -> Command to be executed.
 # Return: 0(True) or 1(False)
 ##################################################################
@@ -15,7 +15,25 @@ function rcmd()
     ssh -t -t -o ConnectTimeout=8 -o StrictHostKeyChecking=no chenyang@$ip "$cmd" && return 0 || return 1 </dev/null
 }
 
+##################################################################
+# Purpose: Get all developers in glue project.
+#          Since all developers have put their name in wiki, we
+#          will filter out their name from wiki txt.
+# Return: NO (will create a file "userlist.txt")
+##################################################################
 function getusers()
 {
     curl -u ci:sp12345678 http://glue.spolo.org/trac/glue/wiki/%E5%BC%80%E5%8F%91%E4%BA%BA%E5%91%98%E4%BF%A1%E6%81%AF?format=txt 2>/dev/null | grep "@spolo.org"|sed -e 's/^.*!\(.*\)@spolo\.org.*$/\1/g' | sed -e '/liwei/d;/hanlu/d;/changyushan/d;/peizhelun/d;/tangying/d' > userlist.txt
+}
+
+##################################################################
+# Purpose: Send email from ci@spolo.org
+# Arguments:
+#   $1 -> Email subject.
+#   $2 -> Email body.
+# Return: NO
+##################################################################
+function xsendmail()
+{
+    echo -e "$2" | mail -s "$1" glue@spolo.org -a "From: Hudson <ci@spolo.org>"
 }
