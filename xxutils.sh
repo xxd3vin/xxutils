790e60293ae1c2a11847a0ed00a2df73ec6349f7
chenyang <chenyang@localhost.localdomain>
2014-09-05 15:50:06 +0800

    Add more format from http://bash.cyberciti.biz/guide/Shell_functions_library

--- xxutils.sh
+++ xxutils.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# run command on remote server
-rcmd()
+##################################################################
+# Purpose: Run command on remote server, return 0 if success.
+# Arguments:
+#   $1 -> IP of remote server
+#   $2 -> Command to be executed.
+# Return: 0(True) or 1(False)
+##################################################################
+function rcmd()
 {
     local ip="$1"
     local cmd="$2"
     echo -e "\n$cmd\n---------------------"
-    ssh -t -t -o ConnectTimeout=8 -o StrictHostKeyChecking=no chenyang@$ip "$cmd"</dev/null
+    ssh -t -t -o ConnectTimeout=8 -o StrictHostKeyChecking=no chenyang@$ip "$cmd" && return 0 || return 1 </dev/null
 }
