2f64bc81e439ba96a3b08d2843fb39c320209533
chenyang <chenyang@localhost.localdomain>
2014-09-05 17:25:53 +0800

    get user list from trac wiki, and format it

--- xxutils.sh
+++ xxutils.sh
@@ -14,3 +14,8 @@ function rcmd()
     echo -e "\n$cmd\n---------------------"
     ssh -t -t -o ConnectTimeout=8 -o StrictHostKeyChecking=no chenyang@$ip "$cmd" && return 0 || return 1 </dev/null
 }
+
+function getusers()
+{
+    curl -u ci:sp12345678 http://glue.spolo.org/trac/glue/wiki/%E5%BC%80%E5%8F%91%E4%BA%BA%E5%91%98%E4%BF%A1%E6%81%AF?format=txt 2>/dev/null | grep "@spolo.org"|sed -e 's/^.*!\(.*\)@spolo\.org.*$/\1/g' | sed -e '/liwei/d;/hanlu/d;/changyushan/d;/peizhelun/d;/tangying/d' > userlist.txt
+}
