aa6dabf3eb6cc7763893e28dd04c285afd9053d5
Devin Chen <d3vin.chen@gmail.com>
2015-07-24 23:57:26 +0800

    install epel

--- setup/centos.sh
+++ setup/centos.sh
@@ -3,3 +3,7 @@
 # base system
 
 yum -y install wget
+
+## RHEL/CentOS 6 64-Bit ##
+wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm -O /tmp/epel-release-6-8.noarch.rpm
+rpm -ivh /tmp/epel-release-6-8.noarch.rpm
