1280149bbe3ee5934bb8a85bbfcd848bec858485
Devin Chen <d3vin.chen@gmail.com>
2015-07-25 00:12:47 +0800

    Update centos.sh

--- setup/centos.sh
+++ setup/centos.sh
@@ -7,3 +7,5 @@ yum -y install wget
 ## RHEL/CentOS 6 64-Bit ##
 wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm -O /tmp/epel-release-6-8.noarch.rpm
 rpm -ivh /tmp/epel-release-6-8.noarch.rpm
+
+install_java_1_6
